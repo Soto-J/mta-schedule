57a7835d79458dcaa331df952ac4bb3af7996203
John Soto <soto.ja415@gmail.com>
2023-11-22 14:42:32 -0500

    Added service status to links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,11 +6,13 @@ const Navbar = () => {
   return (
     <nav className="bg-red-500 h-18 border-b shadow-md p-4">
       <div className="flex justify-between items-center px-10">
-        <div className="">LOGO</div>
+        <Link href="/">LOGO</Link>
+
         <div className="flex gap-x-8 text-lg">
           <Link href="/schedules">Schedules</Link>
           <Link href="/maps">Maps</Link>
           <Link href="/fairs-tolls">Fairs and Tolls</Link>
+          <Link href="/service-status">Service Status</Link>
         </div>
 
         <LightDarkToggle />
