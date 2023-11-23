fa04a743f864ba419590486c237efd24fec11f71
John Soto <soto.ja415@gmail.com>
2023-11-22 23:27:34 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Testing from "@/components/testing";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <Testing />
     </div>
   );
