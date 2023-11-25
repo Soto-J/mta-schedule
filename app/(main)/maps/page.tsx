d9d265d65b270eaf53ba55aed09bccd3bdb9bf2c
John Soto <soto.ja415@gmail.com>
2023-11-24 20:54:36 -0500

    Added geolocation

--- app/(main)/maps/page.tsx
+++ app/(main)/maps/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useEffect, useMemo, useState } from "react";
+
 const MapsPage = () => {
-  return <div>MapsPage</div>;
+  const [position, setPosition] = useState<[number, number]>();
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setPosition([position.coords.latitude, position.coords.longitude]);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(position);
+  }, [position]);
+
+  return (
+    <main>
+      <h2 className="font-bold text-6xl text-center">Maps</h2>
+
+      <div className="mt-10">
+        <Map
+          center={position}
+          className="h-[35vh] w-96 rounded-lg"
+        />
+      </div>
+    </main>
+  );
 };
 
 export default MapsPage;
