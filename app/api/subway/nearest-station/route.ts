2443f7bb5eb03a4de474697100c08addaac33183
John Soto <soto.ja415@gmail.com>
2023-11-25 00:26:20 -0500

    Added nearest station route

---
+++ app/api/subway/nearest-station/route.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+type Params = {
+  params: {
+    station_latitude: number;
+    station_longitude: number;
+  };
+};
+
+export async function GET(req: Request, { params }: Params) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const latitude = searchParams.get("latitude");
+    const longitude = searchParams.get("longitude");
+
+    if (!latitude || !longitude) {
+      return new NextResponse(`Bad Request`, { status: 400 });
+    }
+
+    // const response = await axios.get(
+    //   `https://dev.socrata.com/foundry/data.ny.gov/i9wp-a4ja`
+    // );
+
+    // if (response.status !== 200) {
+    //   return new NextResponse(`${response.statusText}`, {
+    //     status: response.status,
+    //   });
+    // }
+
+    return NextResponse.json({ message: "Hello World" });
+  } catch (error) {
+    throw new NextResponse(`Iternal Error`, { status: 500 });
+  }
+}
+
+/*
+  Given a latitude and longitude, find the nearest subway station
+
+  Documentation:
+  https://dev.socrata.com/foundry/data.ny.gov/i9wp-a4ja
+ 
+  api: 
+  https://data.ny.gov/resource/i9wp-a4ja.json
+*/
