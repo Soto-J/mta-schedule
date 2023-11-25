15eb059f6b1df123413bf0413eded61fe824a7ac
John Soto <soto.ja415@gmail.com>
2023-11-25 18:05:13 -0500

    updating design

--- app/_components/home-tabs.tsx
+++ app/_components/home-tabs.tsx
@@ -1,23 +1,35 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BusFront, TrainFront, TrainTrack, TramFront } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const HomeTabs = () => {
   return (
     <div>
-      <Tabs defaultValue="subway" className="">
-        <TabsList className="space-x-4 px-4 py-10">
-          <TabsTrigger value="subway" className="flex w-20 flex-col">
+      <Tabs defaultValue="subway" className="w-[400px]">
+        <TabsList className="h-15 grid w-full grid-cols-3 content-center">
+          <TabsTrigger value="subway" className="flex flex-col gap-1">
             <span>Subway</span>
             <TramFront />
           </TabsTrigger>
-          <TabsTrigger value="bus" className="flex w-20 flex-col">
+          <TabsTrigger value="bus" className="flex flex-col gap-1">
             <span>Bus</span>
             <BusFront />
           </TabsTrigger>
-          <TabsTrigger value="rail" className="flex w-20 flex-col">
+          <TabsTrigger value="rail" className="flex flex-col gap-1">
             <span>Rail</span>
             <TrainTrack />
           </TabsTrigger>
+
+          <TabsContent value="subway">
+            <Card></Card>
+          </TabsContent>
         </TabsList>
       </Tabs>
     </div>
