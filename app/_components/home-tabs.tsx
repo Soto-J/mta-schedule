a4d45190b0184b640d13210e182b16d0df452b99
John Soto <soto.ja415@gmail.com>
2023-11-27 18:11:18 -0500

    Added card component to tab

--- app/_components/home-tabs.tsx
+++ app/_components/home-tabs.tsx
@@ -11,9 +11,9 @@ import {
 
 const HomeTabs = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-fit">
       <Tabs defaultValue="subway" className="w-[400px]">
-        <TabsList className="h-15 grid w-full grid-cols-3 content-center">
+        <TabsList className="h-15 grid w-full grid-cols-3">
           <TabsTrigger value="subway" className="flex flex-col gap-1">
             <span>Subway</span>
             <TramFront />
@@ -28,6 +28,21 @@ const HomeTabs = () => {
           </TabsTrigger>
 
           <TabsContent value="subway">
+            <Card className="">
+              <div>
+                <CardHeader>Delays</CardHeader>
+              </div>
+              <div>
+                <CardHeader>Planned</CardHeader>
+              </div>
+            </Card>
+          </TabsContent>
+
+          <TabsContent value="bus">
+            <Card></Card>
+          </TabsContent>
+
+          <TabsContent value="rail">
             <Card></Card>
           </TabsContent>
         </TabsList>
