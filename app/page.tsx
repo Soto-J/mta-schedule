e18e3cd94c12be1c99f20573fdbae0e65504aa46
John Soto <soto.ja415@gmail.com>
2023-11-27 16:35:50 -0500

    comment out header

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import HomeTabs from "./_components/home-tabs";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <h2 className="hidden bg-red-500 text-3xl font-semibold md:flex">
+    <div className="">
+      {/* <h2 className="hidden bg-red-500 text-3xl font-semibold md:flex">
         <Link href="/service-status">Service Status</Link>
-      </h2>
+      </h2> */}
 
       <HomeTabs />
 
