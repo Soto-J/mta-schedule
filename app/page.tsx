c5fbab975e7eb24de5f897c60d1612fd2b03ae26
John Soto <soto.ja415@gmail.com>
2023-11-30 00:32:07 -0500

    Added state to transfer data between components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Link from "next/link";
+"use client";
 
-import RouteSearch from "@/components/route-search";
 import HomeTabs from "../components/tabs/home-tabs";
 import ServiceAlertButton from "@/components/service-alert-button";
+import { useState } from "react";
 
 export default function Home() {
+  const [tempState, setTempSate] = useState();
+
   return (
     <div className="">
-      <HomeTabs />
+      <HomeTabs alertData={tempState} />
 
       <div className="mx-auto mt-4 max-w-fit">
-        <ServiceAlertButton />
+        <ServiceAlertButton getData={(data) => setTempSate(data)} />
       </div>
     </div>
   );
