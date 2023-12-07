d656da4a0e21fae5652ebdf30d72bdd57b025d7b
John Soto <soto.ja415@gmail.com>
2023-12-06 23:46:18 -0500

    Style: Added service guides for each borough

--- app/(main)/maps/_components/bus-service-guide.tsx
+++ app/(main)/maps/_components/bus-service-guide.tsx
@@ -2,23 +2,42 @@ import { Download } from "lucide-react";
 
 const BusServiceGuide = () => {
   const SERVICE_GUIDE_LINKS = [
-    { title: "Bronx Bus Service Guide", href: "" },
-    { title: "Brooklyn Bus Service Guide", href: "" },
-    { title: "Manhattan Bus Service Guide", href: "" },
-    { title: "Queens Bus Service Guide", href: "" },
-    { title: "Staten Island Bus Service Guide", href: "" },
-    { title: "Staten Island Expres Bus Service Guide", href: "" },
+    {
+      title: "Bronx Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-bx.pdf",
+    },
+    {
+      title: "Brooklyn Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-bk.pdf",
+    },
+    {
+      title: "Manhattan Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-bk.pdf",
+    },
+    {
+      title: "Queens Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-q.pdf",
+    },
+    {
+      title: "Staten Island Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-si.pdf",
+    },
+    {
+      title: "Staten Island Expres Bus Service Guide",
+      href: "/images/maps/bus/service-guide/service-guide-sie.pdf",
+    },
   ];
 
   return (
     <div className="mt-24">
       <h2 className="text-4xl font-bold">Borough Bus Service Guide</h2>
 
-      <ul className="mt-8 grid md:grid-cols-2 gap-y-5">
+      <ul className="mt-8 grid gap-y-5 md:grid-cols-2">
         {SERVICE_GUIDE_LINKS.map((link) => (
           <li key={link.title}>
             <a
               href={link.href}
+              download={link.title}
               className="
                 flex 
                 max-w-fit
