87a0f77bdeca88691cfd4f0c3476a205ba13ca94
John Soto <soto.ja415@gmail.com>
2023-12-08 14:13:19 -0500

    Refactor: Moved service alert logic to this file

--- app/api/service-status/subway-alerts/route.ts
+++ app/api/service-status/subway-alerts/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
-
 import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
     const response = await axios.get(
       "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts",
@@ -12,7 +11,7 @@ export async function GET() {
         headers: {
           "x-api-key": process.env.MTA_API_KEY,
         },
-      }
+      },
     );
 
     if (response.status !== 200) {
@@ -21,22 +20,84 @@ export async function GET() {
       });
     }
 
-    const decodedSubwayAlerts =
-      GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-        new Uint8Array(response.data)
+    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+      new Uint8Array(response.data),
+    );
+
+    console.log(feed);
+
+    if (!feed) {
+      return new NextResponse(`No data`, { status: 404 });
+    }
+
+    // DELAY ALERTS
+    const delayAlerts = feed.entity.filter((entity) =>
+      entity.id.includes("alert"),
+    );
+
+    const delayedTrains = delayAlerts?.reduce((trains: any, data: any) => {
+      data.alert.informedEntity.forEach((entity: any) => {
+        if (entity.routeId) {
+          trains?.add(entity.routeId);
+        }
+      });
+
+      return trains;
+    }, new Set());
+
+    // PLANNED WORK ALERTS - Window between now and 6 hours later
+    const plannedWorkAlerts = feed.entity.filter((entity) => {
+      // Current time window
+      const currentDate = new Date();
+      const sixHoursLater = new Date(
+        currentDate.getTime() + 6 * 60 * 60 * 1000,
       );
+      // planned work window
+      const plannedStart = new Date(
+        entity.alert?.activePeriod?.[0].start * 1000,
+      );
+      const plannedEnd = new Date(entity.alert?.activePeriod?.[0].end * 1000);
 
-    const subwayAlerts = decodedSubwayAlerts.entity.map(({ alert }) => ({
-      informedEntity: {
-        routeId: alert?.informedEntity?.at(0)?.routeId,
-        stopId: alert?.informedEntity?.at(1)?.stopId,
+      // return (
+      //   entity.id.includes("planned_work") &&
+      //   ((currentDate <= plannedStart && sixHoursLater >= plannedStart) ||
+      //     (currentDate >= plannedStart && currentDate <= plannedEnd))
+      // );
+
+      return (
+        entity.id.includes("planned_work") &&
+        ((currentDate >= plannedStart && sixHoursLater <= plannedEnd) ||
+          (currentDate <= plannedStart && sixHoursLater >= plannedStart) ||
+          (currentDate >= plannedStart && currentDate <= plannedEnd) ||
+          (currentDate <= plannedStart && sixHoursLater >= plannedEnd))
+      );
+    });
+
+    const plannedWorkTrains = plannedWorkAlerts?.reduce(
+      (trains: any, data: any) => {
+        data.alert.informedEntity.forEach((entity: any) => {
+          if (entity.routeId) {
+            trains?.add(entity.routeId);
+          }
+        });
+
+        return trains;
       },
-      activePeriod: alert?.activePeriod,
-      descriptionText: alert?.headerText?.translation?.at(0)?.text,
-    }));
+      new Set(),
+    );
 
-    return NextResponse.json(subwayAlerts);
+    return NextResponse.json({
+      delayFeed: {
+        alerts: delayAlerts,
+        trains: Array.from(delayedTrains),
+      },
+      plannedWorkFeed: {
+        alerts: plannedWorkAlerts,
+        trains: Array.from(plannedWorkTrains),
+      },
+      feed,
+    });
   } catch (error) {
-    return new NextResponse(`Iternal Error`, { status: 500 });
+    throw new NextResponse(`Iternal Error`, { status: 500 });
   }
 }
