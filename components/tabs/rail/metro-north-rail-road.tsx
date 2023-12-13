9aa2b21c6385a4b25de682ad4f1ee8821c80b696
John Soto <soto.ja415@gmail.com>
2023-12-12 21:00:50 -0500

    Feat: Created metro north rail road component

---
+++ components/tabs/rail/metro-north-rail-road.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const MetroNorthRailRoad = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg">Metro North RailRoad</CardTitle>
+      </CardHeader>
+
+      <CardContent>{}</CardContent>
+    </Card>
+  );
+};
+
+export default MetroNorthRailRoad;
