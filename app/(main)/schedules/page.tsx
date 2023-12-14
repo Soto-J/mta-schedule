b4649c7314db94060b5fd5e537d9dd4052cc2da3
John Soto <soto.ja415@gmail.com>
2023-12-13 22:37:12 -0500

    Style: Added p tag

--- app/(main)/schedules/page.tsx
+++ app/(main)/schedules/page.tsx
@@ -1,5 +1,12 @@
 const SchedulePage = () => {
-  return <div>SchedulePage</div>;
+  return (
+    <div>
+      <p className="text-lg">
+        View LIRR and Metro-North schedules and fares and railroad, bus, and
+        subway timetables.
+      </p>
+    </div>
+  );
 };
 
 export default SchedulePage;
