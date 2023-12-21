54343246240e6e02802e4ea5f3b704181c8e9dbf
John Soto <soto.ja415@gmail.com>
2023-12-20 20:27:28 -0500

    Refactor: Removed props

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import HomeTabs from "@/components/tabs/home-tabs";
-import { getRailway } from "./actions/get-railway";
 
 export default async function Home() {
-  const railways = await getRailway();
-
   return (
-    <div className="">
-      <HomeTabs railways={railways} />
+    <div>
+      <HomeTabs />
     </div>
   );
 }
