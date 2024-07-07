8ba1a476cf613f8d99c035ac70714b0570f74e19
John Soto <soto.ja415@gmail.com>
2024-07-07 17:45:14 -0400

    Added outputfileTrachingRoot to include csv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(process.cwd(), "temp/csv"),
+  },
+};
 
 module.exports = nextConfig;
