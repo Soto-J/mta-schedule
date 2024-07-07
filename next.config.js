6e439e68212d7539a4659926b33f2d792028b890
John Soto <soto.ja415@gmail.com>
2024-07-07 17:51:03 -0400

    Replaced outputfileTrachingRoot with output

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/lib/railway-helpers.ts": ["./temp/**/*"],
-    },
-  },
+  // experimental: {
+  //   outputFileTracingIncludes: {
+  //     "/lib/railway-helpers.ts": ["./temp/**/*"],
+  //   },
+  // },
+  output: "standalone/temp",
 };
 
 module.exports = nextConfig;
